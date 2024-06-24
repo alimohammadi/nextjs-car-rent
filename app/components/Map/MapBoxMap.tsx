845b18799a04ec60032598e3de96f157284a24a0
Ali <alimohammadi02321@gmail.com>
2024-06-24 10:08:57 +0330

    Improve/ add map pin icon and connect current loaction to map

--- app/components/Map/MapBoxMap.tsx
+++ app/components/Map/MapBoxMap.tsx
@@ -1,21 +1,46 @@
 "use client";
-import React from "react";
-import { Map } from "react-map-gl";
+import React, { useContext, useRef } from "react";
+import { Map, Marker } from "react-map-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
+import { UserLocationContext } from "@/context/UserLocationContext";
+import "mapbox-gl/dist/mapbox-gl.css";
+import Image from "next/image";
 
 function MapboxMap() {
+  const mapRef = useRef<any>();
+  const { userLocation, setUserLocation } = useContext(UserLocationContext);
+
   return (
-    <Map
-      mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}
-      boxZoom
-      initialViewState={{
-        longitude: -122.4,
-        latitude: 37.8,
-        zoom: 14,
-      }}
-      style={{ width: "100%", height: 400, borderRadius: 12 }}
-      mapStyle="mapbox://styles/mapbox/streets-v9"
-    />
+    <div>
+      {userLocation ? (
+        <Map
+          ref={mapRef}
+          mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}
+          initialViewState={{
+            longitude: userLocation?.lng,
+            latitude: userLocation?.lat,
+            zoom: 14,
+          }}
+          style={{ width: "100%", height: 450, borderRadius: 10 }}
+          mapStyle="mapbox://styles/mapbox/streets-v9"
+        >
+          <Marker
+            longitude={userLocation?.lng}
+            latitude={userLocation?.lat}
+            anchor="bottom"
+          >
+            <Image width={40} height={40} alt="map-pin" src="/pin.png" />
+          </Marker>
+          {/* <Markers />
+
+          {directionData?.routes ? (
+            <MapBoxRoute
+              coordinates={directionData?.routes[0]?.geometry?.coordinates}
+            />
+          ) : null} */}
+        </Map>
+      ) : null}
+    </div>
   );
 }
 
