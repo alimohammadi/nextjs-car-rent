ddb4864fd16f205cdf3329ee589f303ec46e0ab1
Ali <alimohammadi02321@gmail.com>
2024-06-24 09:56:24 +0330

    Feature/ get user location & location context to handle location

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,39 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import Booking from "./components/Booking/Booking";
 import MapboxMap from "./components/Map/MapBoxMap";
+import { UserLocationContext } from "@/context/UserLocationContext";
 
 export default function Home() {
+  const [userLocation, setUserLocation] = useState<any>();
+  const [soruceCordinates, setSourceCordinates] = useState<any>([]);
+  const [destinationCordinates, setDestinationCordinates] = useState<any>([]);
+  const [directionData, setDirectionData] = useState<any>([]);
+
+  const getUserLocation = () => {
+    navigator.geolocation.getCurrentPosition(function (pos) {
+      setUserLocation({
+        lat: pos.coords.latitude,
+        lng: pos.coords.longitude,
+      });
+    });
+  };
+
+  useEffect(() => {
+    getUserLocation();
+  }, []);
+
   return (
-    <section className="grid md:grid-cols-3">
-      <div>
-        <Booking />
-      </div>
-      <div className="col-span-2 max-md:order-first">
-        <MapboxMap />
-      </div>
-    </section>
+    <UserLocationContext.Provider value={{ userLocation, setUserLocation }}>
+      <section className="grid md:grid-cols-3">
+        <div>
+          <Booking />
+        </div>
+        <div className="col-span-2 max-md:order-first">
+          <MapboxMap />
+        </div>
+      </section>
+    </UserLocationContext.Provider>
   );
 }
